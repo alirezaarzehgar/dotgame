4bc92bc093bdda6eb8fb3c7b546cdff5c5552d1b
alireza <alirezaarzehgar82@gmail.com>
2022-11-30 17:18:59 +0330

    main.c: add print matrix prototype
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- src/main.c
+++ src/main.c
@@ -15,13 +15,34 @@ int main(int argc, char const *argv[])
     /* declaration */
 
     /* parse arguments */
+    if (argc == 2 && !strcmp("pro", argv[1], 4))
+        mode = PRO_MODE;
 
     /* initialization */
+    memset(matr, 0, sizeof(matr));
 
     /* start game */
     for (;;)
     {
-        /* show matrix */
+        /* print matrix */
+        for (int i = 0; i < mode; i++)
+        {
+            /* print dots */
+            for (int j = 0; j < mode; j++)
+            {
+                putchar('*');
+                /* -- or "  " */
+            }
+
+            /* print horizental lines */
+            for (int i = 0; i < mode; i++)
+            {
+                /* "   " or "|  " */
+            }
+
+            putchar('\n');
+        }
+
         /* get data */
         /* analyze */
     }
