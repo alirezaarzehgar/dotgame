5a619af4b7867e56d7a51d967eebf2f1ea7205a5
alireza <alirezaarzehgar82@gmail.com>
2022-11-30 17:11:02 +0330

    main.c: add prototype
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#define NORMAL_MODE 4
+#define PRO_MODE 6
+
+static int matr[PRO_MODE][PRO_MODE];
+static int mode = NORMAL_MODE;
+
+int main(int argc, char const *argv[])
+{
+    /* declaration */
+
+    /* parse arguments */
+
+    /* initialization */
+
+    /* start game */
+    for (;;)
+    {
+        /* show matrix */
+        /* get data */
+        /* analyze */
+    }
+
+    /* result */
+
+    return (EXIT_SUCCESS);
+}
