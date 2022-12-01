0170bfa73a6ab76d971f2c042f24270c114932a8
alireza <alirezaarzehgar82@gmail.com>
2022-12-01 05:16:17 +0330

    main.c: support vertical lines
    - Update `matr` array to 3D array. Third dimension is
    about vertical and horizontal player ids.
    - Define HOR & VER constants.
    - Start players from 1 cause to replace `var == -1` conditions
    with `!var`.
    - Replace memset with bzero cause to readablity.
    - Print vertical lines with pipe character.
    - Fix goto label compiler error on some machines. Declaring
    labels before variable declaration is not acceptable on some
    compilers.
    - Remove error colors cause to simplisity.
    - Reimplement duplicated point validation for new matrix.
    - Join storing point section to validation.
    - Refactor next player section.
    
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- src/main.c
+++ src/main.c
@@ -4,22 +4,24 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-#define NORMAL_MODE 4
-#define PRO_MODE 6
-#define NPLAYERS 2
+#define NORMAL_MODE     4
+#define PRO_MODE        6
+#define NPLAYERS        2
+#define HOR             0
+#define VER             1
 
 int main(int argc, char const *argv[])
 {
     /* declaration */
-    int player = 0, mode = NORMAL_MODE;
-    int matr[PRO_MODE][PRO_MODE];
+    int player = 1, mode = NORMAL_MODE;
+    int matr[PRO_MODE][PRO_MODE][2];
 
     /* parse arguments */
     if (argc == 2 && !strncmp("pro", argv[1], 4))
         mode = PRO_MODE;
 
     /* initialization */
-    memset(matr, -1, sizeof(matr));
+    bzero(matr, sizeof(matr));
 
     /* start game */
     for (;;)
@@ -29,24 +31,23 @@ int main(int argc, char const *argv[])
         {
             /* print dots */
             for (int j = 0; j < mode; j++)
-                printf("%d%s", matr[i][j],
-                        matr[i][j+1] != -1 ? "--" : "  ");
+                printf("*%s", matr[i][j+1][HOR] ? "--" : "  ");
+            putchar('\n');
 
             /* print horizental lines */
+            for (int j = 0; j < mode; j++)
+                printf("%c  ", matr[i+1][j][VER] ? '|' : ' ');
             putchar('\n');
-            for (int i = 0; i < mode; i++)
-            {
-                /* "   " or "|  " */
-            }
         }
 
         /* get data */
-    retry:
         int direction, row, col, *point;
-        char *line = NULL;
+        char *line;
         size_t sline = 0;
 
-        printf("Player %c turn.Enter coordinates:\n", 'A' + player);
+    retry:
+        line = NULL;
+        printf("Player %c turn.Enter coordinates:\n", 'A' + player - 1);
         getline(&line, &sline, stdin);
         if (feof(stdin))
         {
@@ -57,25 +58,24 @@ int main(int argc, char const *argv[])
 
         /* input validation */
         row--, col--;
-        point = direction ? &matr[row+1][col] : &matr[row][col+1];
+        point = direction ? matr[row+1][col] : matr[row][col+1];
         if (!(direction == 0 || direction == 1)
-            || (row < 0 || row > mode)
-            || (col < 0 || col > mode)
-            || (*point != -1))
+            || row < 0 || row > mode
+            || col < 0 || col > mode
+            || point[direction])
         {
-            fprintf(stderr, "\033[31mInvalid input. retry\033[0m\n");
+            fprintf(stderr, "Invalid input. retry\n");
             goto retry;
         }
-
-        /* store player lines */
-        *point = player;
+        point[direction] = player;
 
         /* show score */
 
         /* check for continue */
 
         /* next player */
-        player = (player + 1) % NPLAYERS;
+        if (++player > NPLAYERS)
+            player = 1;
     }
 
     /* result */
